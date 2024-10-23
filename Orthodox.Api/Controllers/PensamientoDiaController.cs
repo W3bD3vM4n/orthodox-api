6699f5da59adfa25eba09a592261fc715d50413c
Hassan Rahman <hr-awesome@hotmail.com>
2024-10-23 17:44:20 -0500

    Add missing PensamientoDiaController

--- Orthodox.Api/Controllers/PensamientoDiaController.cs
+++ Orthodox.Api/Controllers/PensamientoDiaController.cs
@@ -1,12 +1,44 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Orthodox.Service.Services;
 
 namespace Orthodox.Api.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class PensamientoDiaController : Controller
     {
-        public IActionResult Index()
+        // Inyección de Dependecias
+        private readonly PensamientoDiaService _pensamientoDiaService;
+
+        public PensamientoDiaController(PensamientoDiaService pensamientoDiaService)
         {
-            return View();
+            _pensamientoDiaService = pensamientoDiaService;
+        }
+
+        [HttpGet]
+        public ActionResult Obtener()
+        {
+            var pensamientoDia = _pensamientoDiaService.ObtenerTodos();
+
+            if (!pensamientoDia.Any())
+            {
+                return NotFound();
+            }
+
+            return Ok(pensamientoDia);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult PorId(int id)
+        {
+            var pensamientoDia = _pensamientoDiaService.ObtenerPorId(id);
+
+            if (pensamientoDia == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(pensamientoDia);
         }
     }
 }
